79ebd2cc011a0fa3e97f69f62dc5afe2f23d5974
madhuvunna <madhuvunna2205@gmail.com>
2024-08-07 19:42:42 +0530

    first

---
+++ src/main/java/com/example/madhu/MadhuApplication.java
@@ -0,0 +1,16 @@
+package com.example.madhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadhuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadhuApplication.class, args);
+	}
+
+		
+
+}
+
